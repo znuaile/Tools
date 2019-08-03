0776682d3aa927e9f7f093c64a737769e3e7d9cd
znuaile <znuaile@hotmail.com>
2019-08-03 15:26:55 +0800

    new log functions

--- addons/pmacros/PersonalMacros.lua
+++ addons/pmacros/PersonalMacros.lua
@@ -6,7 +6,11 @@ if Stubby then
     PMF_DbgMsg =  Stubby.Print
 end
 ]]
-
+PMF_TxtColorRed={0.9,0.1,0.1}
+PMF_TxtColorGreen={0.1,0.9,0.1}
+PMF_TxtColorBlue={0.1,0.1,0.9}
+PMF_TxtColorWhite={1,1,1}
+PMF_TxtColorBlack={0,0,0}
 function PMF_PrintLog(...)
 	if ( DEFAULT_CHAT_FRAME ) then 
 		local msg = ""
@@ -17,6 +21,19 @@ function PMF_PrintLog(...)
         end
         -- msg ,red ,green,blue,messageId,time alive
 		DEFAULT_CHAT_FRAME:AddMessage(msg, 0, 0.2, 0.7)
+	end
+end
+
+function PMF_ColorPrint(colors,...)
+	if ( DEFAULT_CHAT_FRAME ) then 
+		local msg = ""
+		for i=1, table.getn(arg) do
+			if i==1 then msg = arg[i]
+			else msg = msg.." "..arg[i]
+			end
+        end
+        -- msg ,red ,green,blue,messageId,time alive
+		DEFAULT_CHAT_FRAME:AddMessage(msg, colors[1], colors[2], colors[3])
 	end
 end
 
